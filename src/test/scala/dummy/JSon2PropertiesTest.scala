71d46eb334f5c081b4299a829dbf072775e73537
crosson.david@gmail.com <crosson.david@gmail.com>
2014-12-21 20:09:22 +0100

    more tests

--- src/test/scala/dummy/JSon2PropertiesTest.scala
+++ src/test/scala/dummy/JSon2PropertiesTest.scala
@@ -21,31 +21,45 @@ import org.scalatest.FunSuite
 import org.scalatest.ShouldMatchers
 import org.scalatest.junit.JUnitRunner
 
-
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL._
 
 @RunWith(classOf[JUnitRunner])
 class JSon2PropertiesTest extends FunSuite with ShouldMatchers {
 
-  test("basic") {
-    import org.json4s._
-    import org.json4s.native.JsonMethods._
-    
-    val json1 = parse(""" { "x":32,  "n" : [1, 2, 3, 4] } """)  
-    val m = JSon2Properties.toProperties(json1)
+  test("basic 1") {
+    val json = parse("""{"name":"Toy","price":35.35}""")
+    val m = JSon2Properties.toProperties(json)
+    m should have size (2)
+    m should contain("name" -> "Toy")
+    m should contain("price" -> 35.35d)
+  }
+
+  test("basic 2") {
+    val json = parse(""" { "x":32,  "n" : [1, 2, 3, 4] } """)
+    val m = JSon2Properties.toProperties(json)
     m should have size (5)
     m should contain("x" -> 32)
     m should contain("n.1" -> 2)
   }
-  
-  
+
+  test("basic 3") {
+    val json = parse(""" { "a": {"b" : {"c": { "d" : 123 } } }  } """)
+    val m = JSon2Properties.toProperties(json)
+    m should have size (1)
+    m should contain("a.b.c.d" -> 123)
+  }
+
   
   test("test with DSL") {
-    import org.json4s.JsonDSL._
-    
-    val bd = 
+    val bd =
       ("test" -> 123) ~
-      ("sub" -> ("x" -> 1) ~ ("y" -> 2) ) ~
-      ("arr" -> List("10", "20") )
+        ("sub" -> ("x" -> 1) ~ ("y" -> 2)) ~
+        ("arr" -> List("10", "20"))
+        
+    //val json = parse(bd.toString())
+    //info(json.toString())
 
     val m = JSon2Properties.toProperties(bd)
     m should have size (5)
